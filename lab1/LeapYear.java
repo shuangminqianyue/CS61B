08599e4340a4f0b3b37cb0cfd91cd67e205af135
shuang_min <2226051689@qq.com>
2023-08-07 19:12:08 +0800

    test

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,10 +1,22 @@
-/** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+/**
+ * Class that determines whether or not a year is a leap year.
+ * 
+ * @author shuang_min
  */
 public class LeapYear {
+    /** Check the year is leap year or not. */
+    public static boolean isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-    /** Calls isLeapYear to print correct statement.
-     *  @param  year to be analyzed
+    /**
+     * Calls isLeapYear to print correct statement.
+     * 
+     * @param year to be analyzed
      */
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
@@ -30,4 +42,3 @@ public class LeapYear {
         }
     }
 }
-
