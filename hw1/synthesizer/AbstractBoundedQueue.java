fc6506cc46564bf92cc1deaa98e533b4f7641d45
shuang_min <2226051689@qq.com>
2023-08-23 18:55:32 +0800

    fix bug

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -14,22 +14,4 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
         return fillCount;
     }
 
-    @Override
-    public boolean isEmpty() {
-        return BoundedQueue.super.isEmpty();
-    }
-
-    @Override
-    public boolean isFull() {
-        return BoundedQueue.super.isFull();
-    }
-
-    @Override
-    public abstract T peek();
-
-    @Override
-    public abstract T dequeue();
-
-    @Override
-    public abstract void enqueue(T x);
 }
